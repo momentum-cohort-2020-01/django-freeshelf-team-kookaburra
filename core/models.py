bb072ecc9579ebbf46d94803099a80ee4a5dc39a
Marsha <marshakshearin@gmail.com>
2020-02-27 17:59:54 -0500

    creates book and outhor models

---
+++ core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey('Author', on_delete=models.DO_NOTHING)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
