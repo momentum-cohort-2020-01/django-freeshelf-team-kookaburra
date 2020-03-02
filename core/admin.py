fd4c220ca931b7c716e3fcf2b7bccc04a6a70cf6
Marsha <marshakshearin@gmail.com>
2020-03-02 13:10:27 -0500

    updated urls images and titles

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from.models import core
+from.models import Book, Author, Category, Title
+
 
 # Register your models here.
 
-admin.site.register(core)
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Title)
--- core/models.py
+++ core/models.py
@@ -1,15 +1,44 @@
 from django.db import models
-
-# Create your models here.
+from django.utils.text import slugify
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    title = models.ForeignKey('Title', on_delete=models.DO_NOTHING)
+    description = models.TextField(max_length=1000)
     author = models.ForeignKey('Author', on_delete=models.DO_NOTHING)
+    published = models.DateField(auto_now=False)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='core/images/')
+    url = models.URLField(blank=True)
+    category = models.ForeignKey(
+        'Category', on_delete=models.DO_NOTHING, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(null=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
